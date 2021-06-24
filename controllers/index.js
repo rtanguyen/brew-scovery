1d1e42c84e15291a778dc21b6ac3977cbdbcaab7
Ronnie Rios <ronniesmail14@gmail.com>
2021-06-24 16:59:21 -0500

    added the index for controllers

--- controllers/index.js
+++ controllers/index.js
@@ -1,1 +1,12 @@
 // test comment 
+const router = require("express").Router();
+
+const router = require('express').Router();
+
+const apiRoutes = require('./api/');
+// const homeRoutes = require('./home-routes.js');
+
+
+router.use('/api', apiRoutes);
+// router.use('/', homeRoutes);
+module.exports = router;
